61ebb0647e7ad27e0409c4503c16ea6e9f53a387
shawn <shianhian327@gmail.com>
2023-11-02 15:55:26 +0800

    fix .

--- start_commands.sh
+++ start_commands.sh
@@ -1,7 +1,5 @@
 # Set timezone
 ln -sf /usr/share/zoneinfo/Asia/Taipei /etc/localtime
 
-pip install uvicorn gunicorn
-
 # Start 
 gunicorn -w 4 -k uvicorn.workers.UvicornWorker -b 0.0.0.0:8000 chatbot_app:app
