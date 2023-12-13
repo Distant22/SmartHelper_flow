19919c2e5f7a5b4b45ce87ffc4521fe292fd3774
Distant22 <sindistant@gmail.com>
2023-12-13 14:50:30 +0800

    refactor(flowbot):更新政府狀態、重構訊息處理判斷式

--- flowbot.py
+++ flowbot.py
@@ -1,120 +1,18 @@
-from datetime import datetime
 from botbuilder.core import (
     ActivityHandler,
     ConversationState,
     TurnContext,
-    UserState,
-    MessageFactory,
+    UserState
 )
-
-from data_models import ConversationFlow, State, UserFeedback
-from botbuilder.schema import CardAction, ActionTypes, SuggestedActions
-import random
-import time
-
-class ValidationResult:
-    def __init__(
-        self, is_valid: bool = False, value: object = None, message: str = None
-    ):
-        self.is_valid = is_valid
-        self.value = value
-        self.message = message
-
-async def late_response(
-    turn_context: TurnContext, question: str, feedback: UserFeedback, flow: ConversationFlow
-):
-    # 如果使用者沒有要處理報表 or 處理完了，就跑進來這裡
-    if question == "handle_result":
-        
-        feedback.handle_document = 0
-        flow.last_state = State.NONE
-        
-        if random.randint(0,1) == 0:
-            return { "data":"＊執行結果＊", "feedback": False}
-        else:
-            return { "data":"＊執行結果＊", "feedback": True }
-        
-    elif flow.last_state == State.BUSY:
-        
-        # 設定使用者的回應選項：點“停止”便停下 ； 總共做三個階段
-        time.sleep(3)
-        feedback.handle_document += 1
-        if feedback.handle_document == 4:
-            return await late_response(turn_context,"handle_result", feedback, flow)
-        send_msg = MessageFactory.text(question+" ; 目前在階段"+str(feedback.handle_document))
-        send_msg.suggested_actions = SuggestedActions(
-            actions=[
-                CardAction(
-                    title="停止",
-                    type=ActionTypes.im_back,
-                    value="停止"
-                )
-            ]
-        )
-        await turn_context.send_activity(send_msg)  
-        return await late_response(turn_context,"handle_document", feedback, flow)
-    
-    
-"""
-實際報表處理程式碼
-"""
-
-async def handle_document(upload_file):
-    """
-    處理報表函式
-    
-    Return:
-    { data : str , is_feedback : bool }
-    """
-    pass
-
-async def handle_question(question: str):
-    """
-    處理問題函式，呼叫 GPT
-    
-    Return:
-    { data : str , is_feedback : bool }
-    """
-    pass
-
-
-async def handle_response(
-    turn_context: TurnContext, 
-    question: str, 
-    file: bytes,
-    feedback: UserFeedback, 
-    flow: ConversationFlow
-):
-    if file == None:
-        
-        result = await handle_question(question)
-
-        return { "data": result['data'], "feedback": result['is_feedback'] }
-       
-    else:
-       
-        send_msg = MessageFactory.text("處理中...")
-        send_msg.suggested_actions = SuggestedActions(
-            actions=[
-                CardAction(
-                    title="停止",
-                    type=ActionTypes.im_back,
-                    value="停止"
-                )
-            ]
-        )
-        await turn_context.send_activity(send_msg)  
-
-        # 處理文件後會回傳結果(string)以及是否要跟使用者詢問回饋(boolean)
-        result = await handle_document(file) 
-          
-        # 讓狀態回到非忙碌
-        flow.last_state = State.NONE
-
-        return { "data": result['data'], "feedback": result['is_feedback'] }
-
+from data_models import ConversationFlow, Socialism, Liberalism
+from controller import document_controller, feedback_controller, feedback_controller, async_task_controller
+from service import get_history, get_context
+from trendychat.chain.report_chat import ReportChat
+from trendychat.messages import ChatMessage
+import asyncio
 
 class FlowBot(ActivityHandler):
+    
     def __init__(self, conversation_state: ConversationState, user_state: UserState):
         if conversation_state is None:
             raise TypeError(
@@ -124,132 +22,111 @@ class FlowBot(ActivityHandler):
             raise TypeError(
                 "[CustomPromptBot]: Missing parameter. user_state is required but None was given"
             )
-
+        self.is_processing = False
+        self.is_feedback = False
+        
         self.conversation_state = conversation_state
         self.user_state = user_state
-
         self.flow_accessor = self.conversation_state.create_property("ConversationFlow")
-        self.feedback_accessor = self.user_state.create_property("UserFeedback")
+        
+        self.stop_event = asyncio.Event() 
 
     async def on_message_activity(self, turn_context: TurnContext):
-        
-        # 每次使用者傳訊息就進來這裡
         try:
-            feedback = await self.feedback_accessor.get(turn_context, UserFeedback)
             flow = await self.flow_accessor.get(turn_context, ConversationFlow)
-
-            await self._fill_out_user_feedback(flow, feedback, turn_context)
-
-            # Save changes to UserState and ConversationState
+            await self.handle_message(flow, turn_context)
             await self.conversation_state.save_changes(turn_context)
-            await self.user_state.save_changes(turn_context)
+            await self.user_state.save_changes(turn_context)    
         except Exception as e:
             print("Error in message activity:",e)
 
 
-    async def _fill_out_user_feedback(
-        self, flow: ConversationFlow, feedback: UserFeedback, turn_context: TurnContext
+    async def handle_message(
+        self, flow: ConversationFlow, turn_context: TurnContext
     ):
         """
         這個函式會根據使用者目前的狀態調整Bot的回覆內容：
         
-        feedback - 紀錄本次訊息是否向使用者詢問回饋（回覆很好｜普通｜很差）
-        ---------------------------------------------------------
-        0 - 並沒有跟使用者要回饋
-        1 - 跟使用者要回饋，使用者尚未答覆
-        2 - 使用者給出回饋的答覆
-        
-        State - 紀錄Bot的目前訊息處理狀況
-        ---------------------------------------------------------
-        NONE - 預設狀態，接收使用者的問題並處理
-        FEEDBACK - 處理完問題，準備向使用者詢問回饋（僅有一定機率會觸發）
-        BUSY - 處理報表的狀態，處理報表結束後會回到 NONE
-        
+        State 為目前的政府狀態
+        ----------------------------------
+        Socialism - 社會主義｜僅處理一部份訊息
+        Liberalism - 自由主義｜任何訊息都視為新的問題；預設狀態
         """
-        user_input = turn_context.activity.text.strip()  
-        
-        # FEEDBACK 狀態代表使用者給出了回饋 -> 儲存回饋並結束狀態
-        if flow.last_state == State.FEEDBACK and (
-            user_input == "不滿意" or user_input == "普通" or user_input == "滿意"
-        ):
-            await turn_context.send_activity(
-                MessageFactory.text(
-                    f"收到回饋： '{user_input}'"
-                )
-            )
-            feedback.status = 2
-            feedback.user = user_input
-            # 在這邊將 Feedback 資訊存進 data_info
+        user_input = turn_context.activity.text.strip() 
+        print("使用者訊息是「",user_input,"」，目前狀態為",flow.last_state)
 
-            # 重製使用者的狀態
-            feedback.status = 0
-            feedback.user = None
-            flow.last_state = State.NONE
-
-        # None 狀態代表使用者問了問題 -> 呼叫幫手＆詢問使用者回饋
-        elif not flow.last_state == State.BUSY:
+        # 1 - 進入自由主義：處理訊息 -> 處理完後進入社會主義
+        if flow.last_state == "Liberalism" and not self.is_processing :
+            await self.process_question(flow, turn_context, user_input)
             
-            await turn_context.send_activity(
-                MessageFactory.text(f"收到訊息： '{user_input}'")
-            )
+        # 2 - 進入社會主義：根據是否在處理報表決定狀態
+        else:
+            # 3 - 紀錄使用者的回饋
+            if self.is_feedback:
+                self.is_feedback = False
+                await self.process_feedback(flow, turn_context, user_input)
             
-            # 如果使用者要處理報表，讓使用者進入一連串的報表處理階段
-            if user_input == "報表": 
-                flow.last_state = State.BUSY
-                try:
-                    result = await late_response(turn_context,"handle_document", feedback, flow)
-                    if not result == None:
-                        await turn_context.send_activity(result['data'])
-                except Exception as e:
-                    print("Error in handle document:",e)
-                    flow.last_state = State.NONE
-                    feedback.handle_document = 0
-                    return await turn_context.send_activity("伺服器出錯，請重新問問題")
-                
-            # 使用者不是問報表 or 報表執行完了 -> 回傳結果
-            else:
-                result = await late_response(turn_context,"handle_result", feedback, flow)
-                await turn_context.send_activity(result['data'])
-            
-            # 如果 Bot 有想跟使用者要 feedback 便進來這
-            if result['feedback']:
-                reply = MessageFactory.text(feedback.bot)
-                reply.suggested_actions = SuggestedActions(
-                    actions=[
-                        CardAction(
-                            title="不滿意",
-                            type=ActionTypes.im_back,
-                            value="不滿意"
-                        ),
-                        CardAction(
-                            title="普通",
-                            type=ActionTypes.im_back,
-                            value="普通"
-                        ),
-                        CardAction(
-                            title="滿意",
-                            type=ActionTypes.im_back,
-                            value="滿意"
-                        ),
-                    ]
-                )
-                await turn_context.send_activity(reply)
-                feedback.status = 1
-                flow.last_state = State.FEEDBACK
-            else:
-                flow.last_state = State.NONE
-        
-        # BUSY 狀態代表Bot正在忙，無視使用者的任何Input
-        elif flow.last_state == State.BUSY:
-            try:
-                if user_input == "停止":
-                    feedback.handle_document = 0
-                    flow.last_state = State.NONE
-                    return await turn_context.send_activity("報表停止處理。")
+            # 2.a - 正在處理報表：除非使用者喊暫停，否則一律回「在忙」
+            elif self.is_processing:
+                if user_input == "暫停":
+                    self.stop_event.set()
+                    self.is_processing = False
+                    flow.set_state(Liberalism())
                 else:
-                    return await turn_context.send_activity("在忙")
-            except Exception as e:
-                flow.last_state = State.NONE
-                feedback.handle_document = 0
-                print("Error in busy state:",e)
-                return await turn_context.send_activity("伺服器出錯，請重新問問題")
+                    await turn_context.send_activity("在忙")
+            
+            # 2.b - 使用者同意調用報表：開始用報表來處理問題
+            elif user_input == "調用報表":
+                self.is_processing = True  
+                await self.process_document(turn_context, user_input)
+            
+            # 2.c - 使用者不同意調用報表：切回自由主義
+            elif user_input == "不調用報表":  
+                flow.set_state(Liberalism())
+                
+            # 2.d - 使用者直接開始問新問題：處理問題 
+            else:
+                await self.process_question(flow, turn_context, user_input)
+
+
+    async def process_document(self, turn_context: TurnContext, text: str):
+        chat_message = ChatMessage(user_text=text, history=get_history(), context=get_context())
+        response = await document_controller(
+            ReportChat.async_generate_initial_response, { "message" : chat_message }
+        )
+        await turn_context.send_activity(response)
+        response = await document_controller(
+            ReportChat.async_generate_final_response, { "message" : chat_message }
+        )
+        self.is_processing = False 
+        await turn_context.send_activity(response)
+        return
+    
+    
+    async def process_feedback(self, flow: ConversationFlow, turn_context: TurnContext, text: str):
+        
+        # 如果使用者回覆的內容在 FEEDBACK List 當中，就紀錄進資料庫
+        response = await feedback_controller(text)
+        flow.set_state(Liberalism())
+        
+        # 如果使用者直接問新問題，改成處理問題
+        if response == None:
+            response = await self.process_question(flow, turn_context, text)
+            
+        # 回傳結果
+        return await turn_context.send_activity(response)
+    
+
+    async def process_question(self, flow: ConversationFlow, turn_context: TurnContext, text: str):
+        
+        # 設成社會主義
+        flow.set_state(Socialism())
+        self.is_processing = True
+        
+        # 處理問題
+        response = await async_task_controller(turn_context, text)
+
+        self.is_feedback = response["feedback"]
+        self.is_processing = False
+        
+        return await turn_context.send_activity(response["result"])
