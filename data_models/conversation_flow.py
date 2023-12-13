cfe1b0295816ddf2c8e1009621010c527b19ecb5
Distant22 <sindistant@gmail.com>
2023-12-13 14:52:21 +0800

    fix(conversation_flow):更新初始狀態

--- data_models/conversation_flow.py
+++ data_models/conversation_flow.py
@@ -1,12 +1,20 @@
-from enum import Enum
-    
-class State(Enum):
-    NONE = 0
-    FEEDBACK = 1
-    BUSY = 2
+from .state import State, Socialism, Communism, Liberalism
 
 class ConversationFlow:
+    """
+    ConversationFlow 藉由控制 State 來管理目前的對話階段 Bot 應該回覆什麼。
+
+    切換體制：
+    flow.set_state(Liberalism())
+    
+    取得目前體制：
+    flow.last_state 
+    """
     def __init__(
-        self, last_state: State = State.NONE,
+        self, last_state: State = Liberalism()
     ):
-        self.last_state = last_state
+        self.last_state = last_state.__class__.__name__
+    
+    def set_state(self, new_state: State):
+        self.last_state = new_state.__class__.__name__
+        return new_state
