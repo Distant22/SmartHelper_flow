11ab098d16ddf5f4357efb2c716891b4c7f66190
shawn <shianhian327@gmail.com>
2023-12-21 11:51:49 +0800

    add design

---
+++ tmp_design.py
@@ -0,0 +1,21 @@
+""" 
+smarthelper_bot-api
+|-app.py
+|-contollers
+    |-message_controller.py
+|-services
+    |- 
+|-DAO
+    |- status_dao.py
+
+
+
+
+Old:
+smarthelper_bot-api
+|-chat_app.py
+|-bot.py
+|-utils.py
+
+
+"""
